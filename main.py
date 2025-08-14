592b4f4c03a629c2563c19038ab7863979157617
ronidomi <roni.domi001@umb.edu>
2025-08-14 01:51:38 -0400

    added more tools to the agent, it can do basic arithmetic with two floats and greet a person you ask it to greet.

--- main.py
+++ main.py
@@ -6,13 +6,43 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+@tool
+def adder(num1: float, num2: float) -> str:
+    """Useful for performing basic float addition"""
+    print(f"tool called")
+    return (f"The sum of {num1} and {num2} is {num1 + num2}")
+
+@tool
+def subtractor(num1: float, num2: float) -> str:
+    """Useful for performing basic float addition"""
+    print(f"tool called")
+    return (f"The difference of {num1} and {num2} is {num1 - num2}")
+
+@tool
+def multiplier(num1: float, num2: float) -> str:
+    """Useful for performing basic float addition"""
+    print(f"tool called")
+    return (f"The product of {num1} and {num2} is {num1 * num2}")
+
+@tool
+def divider(num1: float, num2: float) -> str:
+    """Useful for performing basic float addition"""
+    print(f"tool called")
+    return (f"The division of {num1} and {num2} is {num1 / num2}")
+
+@tool
+def say_hello(name: str) -> str:
+    """Says hello to a specified name"""
+    return f"Hello, {name}! How are you today?"
+
+
 def main():
     model = ChatOpenAI(
         model="gpt-5-nano",
         temperature=0 # Temperature determines randomness of LLM, 0 - no randomness
     )
 
-    tools = []
+    tools = [adder, subtractor, multiplier, divider, say_hello]
     agent_executor = create_react_agent(model, tools)
 
     print("Hello, I am your AI agent! To stop chatting with me type 'quit'.")
